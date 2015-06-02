cd8438a8fd9b5442248d80233cc35b7ec4f0d8ae
MihaiMindrutescu <muad_dib_of_arakiss@yahoo.com>
2015-06-02 17:21:24 +0300

    Unnecessary pages deleted

--- BestAutoPrj/src/test/java/com/steps/HomeSteps.java
+++ BestAutoPrj/src/test/java/com/steps/HomeSteps.java
@@ -2,12 +2,56 @@ package com.steps;
 
 import net.thucydides.core.steps.ScenarioSteps;
 
-public class HomeSteps extends ScenarioSteps{
+import java.util.List;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.pages.PageObject;
 
-	
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import com.pages.HomePage;
+
+public class HomeSteps {
+
+	HomePage homePage;
+
+	@Step
+	public void selectBrand(String option) {
+		homePage.selectBrandOption(option);
+	}
+
+	@Step
+	public void selectModel(String option) {
+		homePage.selectModelOption(option);
+		;
+	}
+
+	@Step
+	public void selectPrice(String option) {
+		homePage.selectPriceOption(option);
+	}
+
+	@Step
+	public void selectVehiclege(String option) {
+		homePage.selectVehiclegeOption(option);
+	}
+
+	@Step
+	public void selectKilometers(String option) {
+		homePage.selectKilometersOption(option);
+	}
+
+	@Step
+	public void selectFuel(String option) {
+		homePage.selectFuelOption(option);
+	}
+
+	@Step
+	public void clickSearchButton() {
+		homePage.clickSearchButton();
+	}
 }
