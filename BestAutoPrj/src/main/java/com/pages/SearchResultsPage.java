1ff10653626dbd6fc01d1c12c3bf016b0d260cde
razvanratiu <razvanratiu@EVO206.evointern>
2015-06-02 17:44:09 +0300

    First Test Running to see if Ford is found at search results

--- BestAutoPrj/src/main/java/com/pages/SearchResultsPage.java
+++ BestAutoPrj/src/main/java/com/pages/SearchResultsPage.java
@@ -6,6 +6,7 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.thucydides.core.pages.PageObject;
 
 import org.apache.xalan.templates.ElemApplyImport;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -14,17 +15,26 @@ public class SearchResultsPage extends PageObject {
 	@FindBy(id = "results_list")
 	private WebElement searchPageContainer;
 
-	public void checkIfSearchCriteriaAppearsOnResultsPage() {
+	public void checkIfSearchCriteriaAppearsOnResultsPage(String searchText) {
+
+		boolean found = true;
 		element(searchPageContainer).waitUntilVisible();
 
 		List<WebElement> resultListItems = searchPageContainer.findElements(By
-				.cssSelector("div[itemprop='itemListElement']"));
+				.cssSelector("div[class='vehicle_info'] h2 a"));
 
-		for (WebElement webElement : resultListItems) {
+		for (WebElement element : resultListItems) {
 
-			System.out.println("Element: " + webElement.getText());
+			if (element.getText().toLowerCase()
+					.contains(searchText.toLowerCase())) {
+				found = true;
+			} else{
+				found = false;
+			}
+			System.out.println("Element: " + element.getText());
 
 		}
+		Assert.assertTrue("The search criteria is not in the page", found);
 	}
 
 }
